034ece485fa3363a8a68c32d1516d7f43b4ef4fa
ShmalTalk <talie_massachi@brown.edu>
2019-12-28 01:30:32 -0500

    Updates

---
+++ precache-manifest.71be90c7f21e9dcb5c8137abf23d0d2a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bcaaa374deb6287a35207acf85efe9",
+    "url": "/birds/index.html"
+  },
+  {
+    "revision": "757158dde1bfa1aeeb20",
+    "url": "/birds/static/css/main.20dcde53.chunk.css"
+  },
+  {
+    "revision": "5d8cbf8360b5ffe65702",
+    "url": "/birds/static/js/2.7ca2d203.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/birds/static/js/2.7ca2d203.chunk.js.LICENSE"
+  },
+  {
+    "revision": "757158dde1bfa1aeeb20",
+    "url": "/birds/static/js/main.91675431.chunk.js"
+  },
+  {
+    "revision": "cccf7a22b1c462a462e4",
+    "url": "/birds/static/js/runtime-main.6d063b73.js"
+  },
+  {
+    "revision": "24f4ccbf7b22b0bb82a10fa667b70a6d",
+    "url": "/birds/static/media/Hoatzin.24f4ccbf.jpg"
+  },
+  {
+    "revision": "c878fdba19967a0e1df8bbfb39cc65e7",
+    "url": "/birds/static/media/Secretarybird.c878fdba.jpg"
+  },
+  {
+    "revision": "1126f9017c6d2b3a05a610b0600890c3",
+    "url": "/birds/static/media/bee_eater.1126f901.jpg"
+  },
+  {
+    "revision": "e88d9faddd7884ab36b02b466c4db468",
+    "url": "/birds/static/media/eagle.e88d9fad.svg"
+  },
+  {
+    "revision": "14b3c455c18676ebc70abe8b07fced27",
+    "url": "/birds/static/media/finch.14b3c455.jpg"
+  },
+  {
+    "revision": "4f5ac8e898c9644173ef4e38059814c1",
+    "url": "/birds/static/media/frigatebird.4f5ac8e8.jpg"
+  },
+  {
+    "revision": "403b44444f71d5ab85ef9764cef444f6",
+    "url": "/birds/static/media/kakapo.403b4444.jpg"
+  },
+  {
+    "revision": "0e429185ccdaa14c663e96871f92a721",
+    "url": "/birds/static/media/oilbird.0e429185.png"
+  },
+  {
+    "revision": "952949935560fcbc297e53bca2384cbc",
+    "url": "/birds/static/media/prairie_chicken.95294993.jpg"
+  },
+  {
+    "revision": "8a426da6459bae92a37b0231e945143c",
+    "url": "/birds/static/media/red_crossbill.8a426da6.jpg"
+  },
+  {
+    "revision": "0be58cb291e97074e8f78a3691ef415b",
+    "url": "/birds/static/media/ruff.0be58cb2.jpg"
+  },
+  {
+    "revision": "3cd0023fae693fa61b876ff18d350edc",
+    "url": "/birds/static/media/shrike.3cd0023f.jpg"
+  },
+  {
+    "revision": "e8baf91d7875a6ee3182c097ddc1b01f",
+    "url": "/birds/static/media/widowbird.e8baf91d.jpeg"
+  }
+]);
